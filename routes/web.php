1b9aea98df0eee9a3d04d69f661cc2569544391f
davidzzz01 <davidjose001@outlook.com>
2024-08-16 14:57:34 -0300

    Primeiro commit do projeto pessoal

---
+++ routes/web.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::middleware(('admin'))->group(function(){
+Route::get('admin', function(){
+    dd('voce é um admin');
+});
+});
+
+Route::middleware(('client'))->group(function(){
+    Route::get('client', function(){
+        dd('voce é um client');
+    });
+    });
+
+
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
