01d1183b48e7754ef86713abc063444befb82396
davidzzz01 <davidjose001@outlook.com>
2024-08-20 11:22:00 -0300

    atualização

--- app/Http/Controllers/PedidoController.php
+++ app/Http/Controllers/PedidoController.php
@@ -6,19 +6,36 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
+
 class PedidoController extends Controller
 {
 public function dashboard(){
     ///Aqui estou pegando os dados da Model/Tabela do banco e colocando tudo num objeto
-      $pedidos = Pedido::all();
+    $pedidos = DB::select('
+SELECT 
+    pedidos.id AS pedido_id,
+    users.name AS nome_cliente,
+    itens.nome AS nome_item,
+    pedidos.status,
+      pedidos.total,
+    pedidos.created_at AS item_create_at
+  
+FROM 
+    pedidos
+JOIN 
+    users ON pedidos.user_id = users.id
+JOIN 
+    itens ON pedidos.item_id = itens.id
+ORDER BY 
+    pedidos.id,
+    users.name, 
+    pedidos.created_at, 
+    pedidos.total; ');
+
+ 
 
 
-    foreach ($pedidos as $pedido){
-        $data_br = date('d-m-Y',strtotime( $pedido->created_at
-   )); 
-
-}
-///aqui retorno uma view (pagina)
+///aqui retorno uma view
 return view ('Admin.dashboard', compact('pedidos'));
 }
 }
--- resources/views/Admin/dashboard.blade.php
+++ resources/views/Admin/dashboard.blade.php
@@ -1,12 +1,4 @@
-<!DOCTYPE html>
-<html lang="pt-BR">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dashboard</title>
-    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
-</head>
-<body>
+
     <h1 class="text-center mb-4">Olá, este é nosso dashboard</h1>
 
     <div class="container">
@@ -15,6 +7,7 @@
                 <tr>
                     <th scope="col">Código</th>
                     <th scope="col">Nome</th>
+                    <th scope="col">Produto</th>
                     <th scope="col">Valor</th>
                     <th scope="col">Status</th>
                     <th scope="col">Data</th>
@@ -24,18 +17,17 @@
                 @foreach($pedidos as $pedido)
                     <tr>
                         <th scope="row">{{ $pedido->pedido_id }}</th>
-                        <td>{{ $pedido->cliente_nome }}</td>
+                        <td>{{ $pedido->nome_cliente }}</td>
+                        <td>{{ $pedido->nome_item }}</td>
                         <td>{{ $pedido->total }}</td>
                         <td>{{ $pedido->status }}</td>
-                        <td>{{ $pedido->data_br }}</td>
+                        <td>{{ date('d-m-Y', strtotime($pedido->item_create_at)) }}</td>
                     </tr>
                 @endforeach
             </tbody>
         </table>
     </div>
 
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.2/dist/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+   
 </body>
 </html>
