a28ecc4805d14221bcedd8bf359eaa64335a9d33
davidzzz01 <davidjose001@outlook.com>
2024-08-23 14:56:20 -0300

    aaa

--- resources/views/Admin/dashboard.blade.php
+++ resources/views/Admin/dashboard.blade.php
@@ -1,34 +1,5 @@
-<div>
-    <nav class="navbar navbar-expand-lg bg-danger custom-navbar" style="height: 100px;">
-        <div class="container-fluid d-flex justify-content-between">
-            <img style="width:60px;" src="https://cdni.iconscout.com/illustration/premium/thumb/pizza-3517875-2938927.png?f=webp" alt="Logo">
-            <a class="navbar-brand" href="#" style="font-size: 30px;">Restaurant</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav ms-auto" style="margin-right: 10px">
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Nossa história</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Dashboard</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Produtos</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Contato</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
-    
-    
-</div>
 
+<x-NavBar/>
     <h1 class="text-center mb-4 mt-2">Dashboard</h1>
 
     <div class="container">
@@ -78,15 +49,12 @@
         </table>
         
     </div>
-
+<x-Footer/>
    
 </body>
 </html>
 <style>
- .nav-link{color:white;}
-.navbar-brand{color: yellow;font-size:50px;   margin-left: 10px; }
-.custom-navbar .container-fluid {display: flex;justify-content: space-between;align-items: center; 
-}
+ 
  th{
     background-color: #dc3545;
     color: white;
---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,9 @@
+<div>
+    <div>
+        <footer class="text-center mt-5 fixed-bottom" style="background-color:  #dc3545; color: white;">
+            <div class="container-fluid ">
+                <p class="navbar-brand" style="color:white;">&copy; 2024 David Dantas. Todos os direitos reservados.</p>
+            </div>
+        </footer>
+    </div>
+</div>
